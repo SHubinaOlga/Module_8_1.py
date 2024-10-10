5718a72bdf70d312302dbd7375bbe190c38e946c
SHubinaOlga <olga.shubina85@yandex.ru>
2024-10-10 23:03:48 +0300

    Module_8_1.py

--- Module_8_1.py
+++ Module_8_1.py
@@ -2,7 +2,7 @@ def add_everything_up(a, b):
     try:
         return a + b
     except TypeError:
-        return f'{str(a)}, {str(b)}'
+        return str(a) + str(b)
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
